af9ec5c05fc5b8da81042ac653344a9caf5cc28a
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 10:47:19 +0530

    Add files via upload

---
+++ change_peramiter.js
@@ -0,0 +1,8 @@
+const perOne=(friend) => {
+    console.log(`I am busy right Now.I am tolkig to ${friend}. I will call you back. `);
+}
+const perTwo=()=>{
+    console.log("I will call you back.")
+}
+perOne('thapa')
+perTwo();
