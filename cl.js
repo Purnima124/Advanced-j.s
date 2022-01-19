44a7ad1ec00d13de1d8c9495b8e296036072cb6b
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 10:48:14 +0530

    Add files via upload

---
+++ cl.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  // creating an object
+  const person1 = new Person('John');
+  const person2 = new Person('Jack');
+  
+  console.log(person1.name); // John
+  console.log(person2.name); // Jack
+
+
+
