74132fdef51a2b4a2c65476afa32572f1414b857
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 11:10:44 +0530

    Add files via upload

---
+++ map5.js
@@ -0,0 +1,13 @@
+var arr=[
+      {price:"100",product:"Mobile"},
+      {price:"200",product:"leptop"},
+      {price:"300",product:"mick"},
+      {price:"400",product:"PC"},
+    ]
+    var newArr=arr.map(function(value){
+      return value.price
+    
+    })
+    console.log(newArr)
+    
+    
