161da639cf8bea6e1d9f856dd5ac6d0060dcbb7e
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 12:37:16 +0530

    Add files via upload

---
+++ passBy_paramitar.js
@@ -0,0 +1,9 @@
+const perOne=(frind)=> {
+    console.log(`I am busy right Now.I am talking to ${frind},I will call you back.`)
+}
+const perTwo=()=>{
+    console.log(`Hey what's up.I call back you dekha.`)
+}
+perOne("purnima")
+perTwo()
+
