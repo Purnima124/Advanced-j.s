7ca6680ac86db483b0ac180556e911aaafb354ae
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 10:40:25 +0530

    Add files via upload

---
+++ await.js
@@ -0,0 +1,17 @@
+// async function test(){
+//     console.log("2 : Message")
+//     await console.log("3 : Message");
+//     console.log("4 : Message");
+// }
+// console.log("1 : message")
+// test();
+// console.log("5 : Message")
+
+
+async function add(){
+    console.log("Hiii")
+    const responce=await("I am fine and you")
+    console.log(responce)
+}
+add()
+console.log("how are you")
