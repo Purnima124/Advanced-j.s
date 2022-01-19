0639f4a1c538d31ee13af585eee65197a3a23468
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 11:09:50 +0530

    Add files via upload

---
+++ map3.js
@@ -0,0 +1,20 @@
+// var arr=[10,20,30,40,50]
+// newArr=arr.map(function(value){
+//     value++;
+//     return value;
+// })
+// console.log(newArr)
+
+// stu=['kumar','nayak','shnanaj','shreshtha']
+// for(let i of stu){
+//     if(i.includes("k")){
+//         return i
+//     }
+
+// }
+
+console.log(stu)
+var arr=[1,2,3,4,5,6,7,8]
+var newArr=arr.map(a=>a*a)
+console.log(newArr)
+
