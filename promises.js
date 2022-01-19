a3ed2138ef8577ae48af02b424806a1bc2fb6de9
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 12:49:57 +0530

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,9 @@
+const count = true;
+let countValue = new Promise(function (resolve, reject) {
+    if (count) {
+        resolve("There is a count value.");
+    } else {
+        reject("There is no count value");
+    }
+});
+console.log(countValue);
