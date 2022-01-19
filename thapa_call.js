ed0f629c0c06969924b8a3f378e4045b4ef5a657
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 13:11:42 +0530

    Add files via upload

---
+++ thapa_call.js
@@ -0,0 +1,13 @@
+const youtuber1={
+    name:"ThapaTechnical",
+    cantent:"programming",
+    feature:function(rating,support){
+        console.log(`very friendly way of teaching .${this.name} is my fav ${this.cantent} channel1.`)
+    }
+}
+// youtuber1.feature();
+const youtuber2={
+    name:"kuchbhi",
+    cantent:"programming&log"
+}
+youtuber1.feature.call(youtuber2);
