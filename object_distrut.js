8e38d804a0a3a3aef1b27cff048708055ecaa6ea
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 12:34:55 +0530

    Add files via upload

---
+++ object_distrut.js
@@ -0,0 +1,20 @@
+var obj={
+    courseName:"javascript",
+    durition:"Sixmonth",
+    tutor:"Geekyshows"
+}
+console.log(obj.courseName)
+// var {course:Name,durition:time,tutor:tcher}=obj
+// console.log(courseName)
+
+// 2
+
+var obj={
+    courseName:"javascript",
+    durition:"Sixmonth",
+    tutor:"Geekyshows"
+}
+var {courseName,durition,tutor}=obj;
+console.log(courseName)
+
+
