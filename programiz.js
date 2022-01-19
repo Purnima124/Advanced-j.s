0d874d1d318d09db4e155053501ee27be2ea5d1c
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 12:42:11 +0530

    Add files via upload

---
+++ programiz.js
@@ -0,0 +1,12 @@
+// const name = 'Jack';
+// console.log(`Hello ${name}`); 
+// // template literals used with expressions
+// const result = `The sum of  4 + 5 is ${4 + 5}`;
+// console.log(result); 
+// console.log(`${result < 10 ? 'Too low': 'Very high'}`)
+
+const message1 = 'This is a long message\n' + 
+'that spans across multiple lines\n' + 
+'in the code.'
+
+console.log(message1)
