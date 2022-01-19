8b6bd68cc6a697199aec7a4efdb63078a1995cd5
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 10:33:49 +0530

    Add files via upload

---
+++ async_awiat.js
@@ -0,0 +1,8 @@
+async function add(){
+    console.log("sangita:Hiiii")
+    const response=await("ok my dear friend")
+    console.log(response)
+}
+add()
+console.log("sangita:How are you")
+console.log("purnima:I am fine and you")
