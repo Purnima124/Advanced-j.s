d3234fa882b18ef46a13ac4712b013dce0d00784
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 13:06:39 +0530

    Add files via upload

---
+++ setInterval.js
@@ -0,0 +1,20 @@
+// function showTime() {
+
+//     // return new date and time
+//     let dateTime= new Date();
+
+//     // return the time
+//     let time = dateTime.toLocaleTimeString();
+
+//     console.log(time)
+// }
+
+// let display = setInterval(showTime, 5[f5000);
+
+// 2
+
+function greet() {
+    console.log('Hello world');
+}
+
+setInterval(greet, 1000);
