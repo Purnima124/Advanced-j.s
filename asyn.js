5739a888f10fff41113fdc418170561f8fe9e5b8
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 10:32:19 +0530

    Add files via upload

---
+++ asyn.js
@@ -0,0 +1,11 @@
+async function test(){
+    return "Hello";
+}
+test().then((result)=>{
+    console.log(result);
+})
+
+// var add=async()=>"hello"
+// test().then((ok)=>{
+//     console.log(ok)
+// })
