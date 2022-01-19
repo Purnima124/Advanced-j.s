6f59a37ec130cbb9f1cecd950ed5daa9caeae2a6
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 13:07:08 +0530

    Add files via upload

---
+++ spread_operator.js
@@ -0,0 +1,71 @@
+// const arr1 = ['one', 'two'];
+// const arr2 = [...arr1, 'three', 'four', 'five'];
+
+// console.log(arr2); 
+// //  Output:
+// //  ["one", "two", "three", "four", "five"]
+
+// const arr1 = ['one', 'two'];
+// const arr2 = [...arr1, 'three', 'four', 'five'];
+
+// console.log(arr2); 
+//  Output:
+//  ["one", "two", "three", "four", "five"]
+
+
+// second method
+
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+// var arrVal=[5,5,3];
+// console.log(...arrVal)
+// sum(...arrVal);
+
+// let arr=[1,2,3,4];
+// let arr1=[5,6,7,8];
+// let arr2=[9,10,11];
+// let newArr=[...arr,...arr1,...arr2]
+// console.log(newArr)
+
+// 1
+// function sum(a,b,c){
+//     console.log(a+b+c)
+    
+// }
+// var array=[1,2,3,4]
+// sum(...array)
+
+// a=[1,2,3]
+// b=[8,9,10]
+// console.log([...a,2,3,4][...a,b])
+
+
+
+
+
+// spreand operator
+
+// function sum(a,b,c){
+//     console.log(a+b+c)
+    
+// }
+// var array=[1,2,3,4]
+// sum.apply(null,array)
+
+// filter
+// var array=[34,35,36,42]
+// var array2=age.filter(check)
+// console.log(age)
+// function  check(age) {
+//     return age>35
+    
+// }
+// check()
+
+
+a=[1,2,3,4]
+// console.log(...a)
+
+a=[1,2,3,4]
+console.log({...a})
