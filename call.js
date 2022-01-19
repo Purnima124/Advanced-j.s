d462d0add942ec75708ee6b562e6f68ae3ea18e5
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-01-19 11:09:10 +0530

    Add files via upload

--- call.js
+++ call.js
@@ -1,16 +1,72 @@
-const youtuber1={
-    name:"ThapaTechnical",
-    cantent:"programming",
-    feature:function(){
-        console.log(`very friendly way of teaching .${this.name} is my fav ${this.cantent} channel1.`)
-    }
+// function add(){
+//     setTimeout(( )=>{
+//         console.log("you can do anythink")
+//     },2000)
+// }
+// function add2(){
+//     setTimeout(()=>{
+//         console.log("you are very nice")
+//         add()
+//     })
+// }
+// add2()
+
+// async function add(){
+//     console.log("hi how are you fireand");
+//     const response =await("I am fine my diear friend")
+//     console.log(response)
+// }
+// add()
+// console.log("okyy firend")
+
+// var name="purnima";
+// var lastName="kumari";
+// console.log(`My name is ${name} and my last name is ${lastName}`)
+
+// function ok(){
+//     return new Promise (function(resolve,reject){
+//         setTimeout(()=>{
+//             const mdd=true;
+//             if(!mdd){
+//                 console.log("your promise is has been resolve")
+//                 resolve()
+//             }else{
+//                 console.log("your promise is has not been resolve")
+//                 reject()
+//             }
+//         },2000)
+//     })
+
+// }
+// ok().then(function(){
+//     console.log("thanks for resolving")
+// }).catch(function(error){
+//     console.log("o on very bad"+error)
+// })
+
+// Promise
+
+function add(){
+    return new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            const a=true;
+            if (a){
+                console.log("your promise has been resolve")
+                resolve()
+            }
+            else{
+                console.log("your promise has not been resolve")
+                reject()
+            }
+        },2000)
+    })
 }
-// youtuber1.feature();
-const youtuber2={
-    name:"kuchbhi",
-    cantent:"programming&log"
-}
-youtuber1.feature.call(youtuber2);
+add().then(function(){
+    console.log("thanks for resolve")
+}).catch(function(){
+    console.log("very bad bro")
+})
+
 
 
 
